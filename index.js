7e12194d5f54fafdccfdfe697dc2310163a5fea7
rafaelrvs <rafaelvieira1333@gmail.com>
2023-07-27 23:43:16 -0300

    feat

---
+++ index.js
@@ -0,0 +1,8 @@
+let btnCadastro = document.querySelector("#cadastro");
+
+btnCadastro.addEventListener("click",(event) =>{
+  window.location.href="cadastro.html"
+  event.preventDefault();
+
+})
+
--- script.js
+++
@@ -1,6 +0,0 @@
-let btnCadastro = document.querySelector("btn-cadastro");
-
-console.log('love')
-setInterval(()=>{
-  console.log(  'java')
-}0)
